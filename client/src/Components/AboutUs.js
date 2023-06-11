b22bce4aa5036630e5e190e23b945908c0d66137
Abhigna <abhignabollam31@gmail.com>
2023-06-11 08:42:05 +0530

    committed

--- client/src/Components/AboutUs.js
+++ client/src/Components/AboutUs.js
@@ -1,11 +1,103 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Navbar from './User/UserNavBar/Navbar'
 
 function AboutUs() {
+  const [expandedQuestion, setExpandedQuestion] = useState(null);
+
+  const toggleQuestion = (questionIndex) => {
+    if (expandedQuestion === questionIndex) {
+      setExpandedQuestion(null);
+    } else {
+      setExpandedQuestion(questionIndex);
+    }
+  };
+
+  const questions = [
+    {
+      question: "Will it hurt when you insert the needle?",
+      answer: 'Only for a moment. Pinch the fleshy, soft underside of your arm. That pinch is similar to what you will feel when the needle is inserted.' 
+    },
+    {
+      question: 'How long does a blood donation take?',
+      answer: 'The entire process takes about one hour and 15 minutes; the actual donation of a pint of whole blood unit takes eight to 10 minutes. However, the time varies slightly with each person depending on several factors including the donor’s health history and attendance at the blood drive.',
+    },
+    {
+      question: 'How long will it take to replenish the pint of blood I donate?',
+      answer: 'The plasma from your donation is replaced within about 24 hours. Red cells need about four to six weeks for complete replacement. That’s why at least eight weeks are required between whole blood donations.',
+    },
+    {
+      question: 'Why does the Red Cross ask so many personal questions when I give blood?',
+      answer: 'The highest priorities of the Red Cross are the safety of the blood supply and our blood donors. Some individuals may be at risk of transferring communicable disease through blood donation due to exposure via travel or other activities or may encounter problems with blood donation due to their health. We ask these questions to ensure that it is safe for patients to receive your blood and to ensure that it is safe for you to donate blood that day.',
+    },
+    {
+      question: 'How often can I donate blood?',
+      answer: 'You must wait at least eight weeks (56 days) between donations of whole blood and 16 weeks (112 days) between Power Red donations. Whole blood donors can donate up to 6 times a year. Platelet apheresis donors may give every 7 days up to 24 times per year. Regulations are different for those giving blood for themselves (autologous donors).',
+    },
+    {
+      question: 'Who can donate blood?',
+      answer: 'In most states, donors must be age 17 or older. Some states allow donation by 16-year-olds with a signed parental consent form. Donors must weigh at least 110 pounds and be in good health. Additional eligibility criteria apply.',
+    },
+    {
+      question: 'Can I bring guests or children with me to my donation appointment?',
+      answer: 'At this time, we are allowing additional guests or children to accompany donors to their donation appointment. Guests are expected to follow any safety protocols in place at the time of donation. The safety of our donors, volunteers, and employees is of the utmost importance.  Children who do not require supervision and are not disruptive are welcome to sit in the waiting or refreshment area.  If they require supervision another adult must be present.',
+    },
+  ];
   return (
-    <div>
-        <Navbar/>
-        AboutUs</div>
+    
+    
+  
+        <>
+    <Navbar/>
+    <br/>
+      <div className="container-fluid">
+     
+     <div className='add'>
+      <h1 className='my-5 '>FAQs</h1>
+      <hr/>
+      {questions.map((q, index) => (
+      <div className="question" key={index} onClick={() => toggleQuestion(index)}>
+        <h4>
+          {q.question}<span className='mx-3'></span> {expandedQuestion === index ? "-" : "+"}
+        </h4>
+        {expandedQuestion === index && <div className="answer">{q.answer}</div>}
+      </div>
+    ))}
+   
+      </div>
+      <br/>
+      <style>
+        {`
+        .container-fluid{
+          width:75%;
+         
+        }
+        .add{
+          border:2px solid black
+        }
+          
+         h1{
+          text-align:center;
+         }
+
+         
+          .question {
+            cursor: pointer;
+            border-bottom: 1px solid #ccc;
+            padding: 10px;
+            background-color: #f9f9f9;
+            
+          }
+
+          .answer {
+            display: block;
+            padding: 10px;
+          }
+        `}
+      </style>
+    </div>
+    </>
+
+
   )
 }
 
--- client/src/Components/Organizations/BloodBank/AcceptorList.js
+++ client/src/Components/Organizations/BloodBank/AcceptorList.js
@@ -1,12 +1,253 @@
-import React from 'react'
-import BloodbankNavBar from './BloodbankNavBar'
+
+
+
+
+import React, { useState } from 'react';
+import BloodbankNavBar from './BloodbankNavBar';
 
 function AcceptorList() {
+  const [request, setRequest] = useState(false);
+ 
+    let data = [
+      {
+               name:"Abhigna",
+               phoneno:9000400640,
+               place:"Karimnagar"
+             },
+             {
+               name:"Vivek",
+               phoneno:9182012045,
+               place:"Karimnagar"
+             },
+             {
+               name:"Ramya",
+               phoneno:9000400640,
+               place:"Karimnagar"
+             },
+             {
+               name:"Vani",
+               phoneno:9182012045,
+               place:"Karimnagar"
+             },
+             {
+               name:"Mallika",
+               phoneno:9000400640,
+               place:"Karimnagar"
+             },
+             {
+               name:"Anil",
+               phoneno:9182012045,
+               place:"Karimnagar"
+             },
+             {
+               name:"Reethika",
+               phoneno:6300567818,
+               place:"Khammam"
+             },
+             {
+               name:"Prathyush",
+               phoneno:9253794719,
+               place:"Khammam"
+             },
+             {
+               name:"Ankith",
+               phoneno:6300567818,
+               place:"Khammam"
+             },
+             {
+               name:"Anvith",
+               phoneno:9253794719,
+               place:"Khammam"
+             },
+             {
+               name:"Ramani",
+               phoneno:6300567818,
+               place:"Khammam"
+             },
+             {
+               name:"Prathyusha",
+               phoneno:9253794719,
+               place:"Khammam"
+             },
+             {
+               name:"Radhika",
+               phoneno:6300567818,
+               place:"Khammam"
+             },
+             {
+               name:"Pallavi",
+               phoneno:9253794719,
+               place:"Khammam"
+             },
+             {
+               name:"prasad",
+               phoneno:965321789,
+               place:"Hyderabad"
+             },
+             {
+               name:"Mudrika",
+               phoneno:7642931990,
+               place:"Hyderabad"
+             },
+             {
+               name:"Sumathi",
+               phoneno:9345621785,
+               place:"Hyderabad"
+             },
+             {
+               name:"Priya",
+               phoneno:8309030130,
+               place:"Hyderabad"
+             },
+             {
+               name:"amulya",
+               phoneno:9345621785,
+               place:"Hyderabad"
+             },
+             {
+               name:"karthika",
+               phoneno:8309030130,
+               place:"Hyderabad"
+             },
+             {
+               name:"Soumya",
+               phoneno:9345621785,
+               place:"Hyderabad"
+             },
+             {
+               name:"Likitha",
+               phoneno:8309030130,
+               place:"Hyderabad"
+             },
+             {
+               name:"spoorthi",
+               phoneno:9542012045,
+               place:"Hyderabad"
+             },
+             {
+               name:"kalyani",
+               phoneno:9345621785,
+               place:"Hyderabad"
+             },
+             {
+               name:"yashi",
+               phoneno:8309030130,
+               place:"Hyderabad"
+             },
+             {
+               name:"gayatri",
+               phoneno:9542012045,
+               place:"Hyderabad"
+             },
+             {
+               name:"Arsiya",
+               phoneno:7629888421,
+               place:"Warangal"
+             },
+             {
+               name:"Ramu",
+               phoneno:8765432154,
+               place:"Warangal"
+             },
+             {
+               name:"Vennela",
+               phoneno:7629888421,
+               place:"Warangal"
+             },
+             {
+               name:"Ramesh",
+               phoneno:8765432154,
+               place:"Warangal"
+             },
+             {
+               name:"Archana",
+               phoneno:7629888421,
+               place:"Warangal"
+             },
+             {
+               name:"Raghu",
+               phoneno:8765432154,
+               place:"Warangal"
+             },
+             {
+               name:"Sumathi",
+               phoneno:9345621785,
+               place:"Nizamabad"
+             },
+             {
+               name:"Priya",
+               phoneno:8309030130,
+               place:"Nizamabad"
+             },
+             {
+               name:"amulya",
+               phoneno:9345621785,
+               place:"Nizamabad"
+             },
+             {
+               name:"karthika",
+               phoneno:8309030130,
+               place:"Nizamabad"
+             },
+             {
+               name:"Soumya",
+               phoneno:9345621785,
+               place:"Nizamabad"
+             },
+             {
+               name:"Likitha",
+               phoneno:8309030130,
+               place:"Nizamabad"
+             },
+             {
+               name:"spoorthi",
+               phoneno:9542012045,
+               place:"Nizamabad"
+             },
+             {
+               name:"kalyani",
+               phoneno:9345621785,
+               place:"Nizamabad"
+             },
+             {
+               name:"yashi",
+               phoneno:8309030130,
+               place:"Nizamabad"
+             },
+             {
+               name:"gayatri",
+               phoneno:9542012045,
+               place:"Nizamabad"
+             },
+    ];
+  
+
   return (
     <div>
-        <BloodbankNavBar/>
-        AcceptorList</div>
-  )
+      <BloodbankNavBar />
+      <br />
+      <div className="container">
+        <table className="table table-bordered table-striped mt-5">
+          <thead className="thead-dark">
+            <tr>
+              <th>Acceptor Name</th>
+              <th>Contact</th>
+              <th>Location</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((bank, index) => (
+              <tr key={index}>
+                <td>{bank.name}</td>
+                <td>{bank.phoneno}</td>
+                <td>{bank.place}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default AcceptorList
+export default AcceptorList;
